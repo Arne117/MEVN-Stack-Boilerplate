a069cad6a6063e5079f3fbcb94768045b5335152
Arne <arne.rolf92@gmail.com>
2018-06-01 23:23:36 +0200

    Update

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
   .p-md-5.m-md-3
+    img(src='@/assets/logo.png')
+    //- img(src='/static/Rooms/SVG/gym.svg')
     .col-md-5.p-lg-5.mx-auto.my-2
       h1 A / B Test check
       p.lead(for='url') Geben sie die URL der Webseite ein
