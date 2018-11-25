e21f6462e9d48c087aa0cbb3ee1955b17daf1e6d
Arne <arne.rolf92@gmail.com>
2018-11-25 18:02:19 +0100

    Survey.js Editor integration

--- client/src/components/survey/CreateSurvey.vue
+++ client/src/components/survey/CreateSurvey.vue
@@ -1,49 +1,42 @@
 <template lang='pug'>
   .Survey-create
     p Create
-    form(v-on:submit.prevent='')
-      input(
-        type='text'
-        name='title'
-        placeholder='Title'
-        v-model='title'
-        maxlength='120'
-        @keyup.enter='addSurvey'
-        autofocus
-      )
-      button(@click='addSurvey') Create
+    SurveyEditor
 </template>
 
 <script>
-import SurveyService from '@/services/SurveyService'
+import SurveyEditor from './SurveyEditor'
+import * as SurveyVue from 'survey-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+
+let Survey = SurveyVue.Survey
+Survey.cssType = 'bootstrap'
 
 export default {
-  data () {
-    return {
-      title: '',
-      survey: {}
-    }
+  components: {
+    Survey,
+    SurveyEditor
   },
-  methods: {
-    async addSurvey () {
-      const response = await SurveyService.addSurvey({
-        title: this.title,
-        survey: this.survey
-      })
-      console.log(response)
-      if (response.status === 201) {
-        this.$swal({
-          title: 'Great!',
-          text: 'Your survey has been added.',
-          type: 'success',
-          confirmButtonText: 'ok'
-        }).then(() => { this.$router.push({ name: 'EditSurvey', params: {id: response.data._id} }) })
-        //
-      }
+  data () {
+    let json = {
+      elements: [
+        { type: 'text', name: 'customerName', title: 'What is your name?', isRequired: true }
+      ]
+    }
+    // Create the model and pass it into VueSJ Survey component
+    let model = new SurveyVue.Model(json)
+    // You may set model properties
+    // model.mode='display'
+
+    return {
+      survey: model
     }
   }
 }
 </script>
 
 <style lang='stylus' scoped>
+  .SurveyEditor {
+    text-align left
+  }
 </style>
--- client/src/components/survey/SurveyEditor.vue
+++ client/src/components/survey/SurveyEditor.vue
@@ -1,18 +1,59 @@
 <template lang='pug'>
   .SurveyEditor
+    #surveyEditorContainer
 </template>
 
 <script>
-// import SurveyService from '@/services/SurveyService'
+import SurveyService from '@/services/SurveyService'
+
+import * as SurveyEditor from 'surveyjs-editor'
+import 'surveyjs-editor/surveyeditor.css'
+import * as SurveyKo from 'survey-knockout'
+
+import * as widgets from 'surveyjs-widgets'
+import 'inputmask/dist/inputmask/phone-codes/phone.js'
+widgets.icheck(SurveyKo)
+widgets.select2(SurveyKo)
+widgets.inputmask(SurveyKo)
+widgets.jquerybarrating(SurveyKo)
+widgets.jqueryuidatepicker(SurveyKo)
+widgets.nouislider(SurveyKo)
+widgets.select2tagbox(SurveyKo)
+widgets.signaturepad(SurveyKo)
+widgets.sortablejs(SurveyKo)
+widgets.ckeditor(SurveyKo)
+widgets.autocomplete(SurveyKo)
+widgets.bootstrapslider(SurveyKo)
 
 export default {
+  name: 'survey-editor',
   data () {
-    return {
-      title: '',
-      survey: {}
+    return {}
+  },
+  mounted () {
+    let editorOptions = { showEmbededSurveyTab: false }
+    this.editor = new SurveyEditor.SurveyEditor('surveyEditorContainer', editorOptions)
+    this.editor.saveSurveyFunc = function () {
+      console.log((this))
     }
   },
-  methods: {}
+  methods: {
+    async addSurvey () {
+      const response = await SurveyService.addSurvey({
+        survey: this.survey
+      })
+      console.log(response)
+      if (response.status === 201) {
+        this.$swal({
+          title: 'Great!',
+          text: 'Your survey has been added.',
+          type: 'success',
+          confirmButtonText: 'ok'
+        }).then(() => { this.$router.push({ name: 'EditSurvey', params: {id: response.data._id} }) })
+        //
+      }
+    }
+  }
 }
 </script>
 
