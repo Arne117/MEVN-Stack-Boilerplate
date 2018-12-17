f0756ca1b175aa85cb85f24f93021c12d7196f99
Arne <arne.rolf92@gmail.com>
2018-12-17 19:11:54 +0100

    Visual Editor

--- client/src/components/survey/visualEditor/Index.vue
+++ client/src/components/survey/visualEditor/Index.vue
@@ -1,8 +1,9 @@
 <template lang='pug'>
-  .VisualEditor(v-if='getSurvey')
-    .EditArea(@click='setActivePage(null, $event)')
+  .VisualEditor(v-if='surveyData')
+    .EditArea(@click.self='setEditAreaActive($event)')
       vue-drag-res.Page(
-        v-for='page, i in getSurvey.pages'
+        v-for='page, i in surveyData.pages'
+        :class='activePage === i ? "active" : ""'
         :parent='true'
         :resizable='false'
         :x='page.position ? page.position.x : 25'
@@ -11,13 +12,13 @@
         :minh='400'
         :grid='[12.5, 12.5]'
         :key='page.name'
-        @activated='onActivate(i)'
-        @deactivated="onDeactivate"
-        @dragging='onDrag'
-        @dragstop='onDragStop'
+        @activated='onPageDragActivate(i)'
+        @deactivated="onPageDragDeactivate"
+        @dragging='onPageDrag'
+        @dragstop='onPageDragStop'
         drag-handle='.DragHandle'
       )
-        .Page-container(@click='setActivePage(i, $event)')
+        .Page-container(@click.self='setPageActive(i, $event)')
           .Page-header
             i.DragHandle.fa.fa-arrows(aria-hidden='true')
             h4 {{ page.title }}
@@ -26,12 +27,20 @@
               element='ul'
               v-model='page.elements'
               :options='dragOptions'
-              :move='onMove'
+              :move='onQuestionMove'
               @start='isDragging=true'
               @end='isDragging=false'
             )
               transition-group.Question-transition(type='transition' :name='"flip-list"')
-                li.Question-item(v-for='question, j in page.elements' :key='question.name') {{ question.title[getSurvey.locale] }}
+                li.Question-item(
+                  v-for='question, j in page.elements' :key='question.name'
+                  :class='activeQuestion.name === question.name ? "active" : ""'
+                  @click.self='setQuestionActive(question, $event)'
+                  ) {{ question.title[surveyData.locale] }}
+                  .QuestionDetails
+                    .QuestionDetails-container
+                      .QuestionDetails-header
+                      .QuestionDetails-body
                   //- component(:is='question.type') {{ question.type }}
                   //- checkbox
 
@@ -42,22 +51,48 @@
     //- Sidebar
     .Sidebar
       .Sidebar-container
-        .Sidebar-questions(
-          :class='sidebarContent === "questions" ? "active" : ""')
+        .Sidebar-questionDetails(:class='sidebarContent === "questionDetails" ? "active" : ""')
+          .Sidebar-header
+            h5 Question details
+          .Sidebar-body
+            .Sidebar-settings(v-if='activeQuestion.title')
+              label(for='questionTitle') Question title
+              input(v-model='activeQuestion.title[surveyData.locale]' name='questionTitle')
+              label(for='questionName') Question name
+              input(v-model='activeQuestion.name' name='questionName')
+              label(for='questionName') Question name
+
+        .Sidebar-questions(:class='sidebarContent === "questions" ? "active" : ""')
           .Sidebar-header
             h5 Questiontypes
           .Sidebar-body
-            .Sidebar-question(v-for='question, i in Survey.ElementFactory.Instance.getAllTypes()') {{ question }}
-        .Sidebar-pages(:class='sidebarContent === "pages" ? "active" : ""')
+            draggable.Question-list(
+              element='ul'
+              v-model='Survey.ElementFactory.Instance.getAllTypes()'
+              :options='{group:{ pull:"clone", put:false }, sort: false}'
+              :move='onQuestionMove'
+              @start='isDragging=true'
+              @end='isDragging=false'
+            )
+              transition-group.Question-transition(type='transition' name='no')
+                li.Sidebar-question.Question-item(v-for='question, i in Survey.ElementFactory.Instance.getAllTypes()' :key='question') {{ question }}
+        .Sidebar-general(:class='sidebarContent === "general" ? "active" : ""')
           .Sidebar-header
-            h5 Pages
+            h5 Settings
           .Sidebar-body
+            .Sidebar-settings
+              label(for='surveyTitle') Survey title
+              input(v-model='surveyData.title' name='surveyTitle')
+            button.Button(@click='addNewPage') New page
 </template>
 
 <script>
 import * as SurveyVue from 'survey-vue'
 import draggable from 'vuedraggable'
-import { mapGetters, mapMutations } from 'vuex'
+// import { mapGetters, mapMutations } from 'vuex'
+import { mapMutations } from 'vuex'
+
+import SurveyService from '@/services/SurveyService'
 
 window.Survey = SurveyVue
 let Survey = SurveyVue.Survey
@@ -71,15 +106,16 @@ export default {
     draggable
   },
   computed: {
-    ...mapGetters([
-      'getSurvey'
-    ])
+    // ...mapGetters([
+    //   'getSurvey'
+    // ])
   },
   data () {
     return {
-      activePage: 0,
-      sidebarContent: 'pages',
-      surveyData: {},
+      activePage: undefined,
+      activeQuestion: {},
+      sidebarContent: 'general',
+      surveyData: undefined,
       Survey: SurveyVue,
       dragOptions: {
         ghostClass: 'Question-item--moving'
@@ -90,29 +126,67 @@ export default {
     ...mapMutations([
       'setSurvey'
     ]),
-    onActivate (activePage) {
-      this.activePage = activePage
-      this.sidebarContent = 'questions'
+    addNewPage () {
+      this.surveyData.pages.push({
+        title: `Seite ${this.surveyData.pages.length + 1} Titel`,
+        name: `Seite ${this.surveyData.pages.length + 1}`,
+        position: this.getPagePosition(),
+        elements: []
+      })
     },
-    onDeactivate () {
-      this.activePage = 0
-      this.sidebarContent = 'pages'
+    async getSurvey () {
+      const response = await SurveyService.getSurvey({
+        id: this.$route.params.id
+      })
+      this.surveyData = response.data
     },
-    onDrag (x, y) {},
-    onDragStop (x, y) {
-      // this.survey.pages[this.activePage].position.x = x
-      // this.survey.pages[this.activePage].position.y = y
-      // console.log(this.survey)
-      // this.setSurvey(this.survey)
+    getPagePosition () {
+      const length = this.surveyData.pages.length
+      const pages = this.surveyData.pages
+      const y = length % 3 === 0 ? pages[length - 1].position.y + 425 : pages[length - 1].position.y
+      if (length % 3 === 0) return { x: 25, y }
+      if (length % 3 === 1) return { x: 375, y }
+      if (length % 3 === 2) return { x: 725, y }
     },
-    setActivePage (activePage, $event) {
-      document.querySelectorAll('.Page').forEach(el => el.classList.remove('active'))
-      if ($event.target.classList.contains('EditArea')) return
-      $event.target.parentNode.classList.add('active')
-      this.activePage = activePage
-      this.sidebarContent = 'questions'
+    onPageDragActivate (activePage) {
+      // this.activePage = activePage
+      // this.sidebarContent = 'questions'
     },
-    onMove () {}
+    onPageDragDeactivate () {
+      // this.activePage = undefined
+      // this.sidebarContent = 'general'
+    },
+    onPageDrag (x, y) {},
+    onPageDragStop (x, y) {
+      this.surveyData.pages[this.activePage].position.x = x
+      this.surveyData.pages[this.activePage].position.y = y
+      this.setSurvey(this.surveyData)
+    },
+    onQuestionMove () {},
+    setPageActive (activePage, $event) {
+      if ($event.target.classList.contains('Question-item')) {
+        this.activePage = activePage
+        this.sidebarContent = 'questionDetails'
+      } else {
+        this.activePage = activePage
+        this.activeQuestion = {}
+        this.sidebarContent = 'questions'
+      }
+    },
+    setEditAreaActive ($event) {
+      if ($event.target.classList.contains('EditArea')) {
+        this.activePage = undefined
+        this.activeQuestion = undefined
+        this.sidebarContent = 'general'
+      }
+    },
+    setQuestionActive (activeQuestion, $event) {
+      this.activeQuestion = activeQuestion
+      this.sidebarContent = 'questionDetails'
+    }
+  },
+  mounted () {
+    this.getSurvey()
   }
 }
 </script>
@@ -121,7 +195,7 @@ export default {
 .VisualEditor {
   display flex
   flex-direction row
-  min-height 1000px
+  min-height 1300px
 
   .EditArea {
     position relative
@@ -168,33 +242,50 @@ export default {
           cursor grabbing
         }
       }
+    }
+  }
 
-      .Question {
-        &-list {
+  .Question {
+    &-list {
+      margin-top .5em
+    }
 
-        }
+    &-transition {
+      display flex
+      flex-direction column
+    }
 
-        &-transition {
+    &-item {
+      padding .5em
+      margin-bottom .5em
+
+      border 1px solid transparent
+      background-color $lightBlue
+      border-radius 10px
+      cursor pointer
+
+      &.active {
+        border 1px solid $darkBlue
+
+        .Question-details {
           display flex
-          flex-direction column
-        }
-
-        &-item {
-          padding .5em
-
-          border-bottom 1px solid $darkBlue
-
-          &--moving {
-            opacity 0.5
-            background $lightGrey
-          }
         }
       }
 
-      .flip-list-move {
-        transition transform 0.5s
+      &--moving {
+        opacity 0.5
+        background $lightGrey
+        cursor grabbing
       }
     }
+
+    &-details {
+      display none
+    }
+  }
+
+  .flip-list-move {
+    transition transform 0.5s
   }
 
   .Sidebar {
@@ -207,7 +298,9 @@ export default {
       padding 1em
     }
 
-    &-questions {
+    &-general,
+    &-questions,
+    &-questionDetails {
       display none
 
       &.active {
@@ -215,11 +308,12 @@ export default {
       }
     }
 
-    &-pages {
-      display none
+    &-settings {
+      margin 1em 0
 
-      &.active {
-        display block
+      label, input {
+        text-align left
+        width 100%
       }
     }
   }
